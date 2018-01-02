2f401c3e42084562a6e51f84881ef95f8e861a42
Nicholas McDermott <nmcdermott89@gmail.com>
2018-01-02 05:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function happyHolidays() {
 return "Happy holidays!"
 }
 
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}"
+}
+
 function happyHolidayTo(holiday, name) {
 return "Happy ${holiday}, ${name}"
 }
+
